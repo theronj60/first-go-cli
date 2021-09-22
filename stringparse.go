3ab4ea50b27664bfaf9b165898583f5d412b7909
Theron Joe <theronj@joetwebdev.io>
2021-09-21 23:34:43 -0600

    started go cli, WIP

--- stringparse.go
+++ stringparse.go
@@ -1,8 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+)
 
 func main() {
 	// cli start
-	fmt.Println("Hello, World!")
+	textPtr := flag.String("text", "", "Text to parse.")
+	metricPtr := flag.String("metric", "chars", "Metric {chars|words|lines};.")
+	uniquePtr := flag.Bool("unique", false, "Measure unique values of a metric.")
+	flag.Parse()
+
+	if *textPtr == "" {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	fmt.Printf("textPtr: %s, metricPtr: %s, uniquePtr: %t\n", *textPtr, *metricPtr, *uniquePtr)
 }
